b562c21dacb475e48390446d8ab24517ef89d771
杨建托 <44236353+flyingTodream@users.noreply.github.com>
2022-12-05 12:06:57 +0800

    fix: init

--- app.js
+++ app.js
@@ -4,9 +4,18 @@ const { getPlayUrl } = require('./getUrl')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    getPlayUrl()
-    res.send('Hello World!')
+app.get('/', async (req, res) => {
+    const { songName, artistsName } = req.body
+    let d = null
+    try {
+        d = await getPlayUrl(songName, artistsName)
+    } catch (error) {
+
+    }
+    res.send({
+        status: 200,
+        data: d
+    })
 })
 
 app.listen(port, () => {
